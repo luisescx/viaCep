52d5e806311f5828e11ba11b4ae671d128bcf654
luisescx <luis.esculapio@live.com>
2022-05-20 16:07:44 -0300

    feat: added companycard component

---
+++ src/components/CompanyCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+      <q-item-label class="text-subtitle1">Surreal São Paulo</q-item-label>
+      <q-item-label class="text-caption">VTEX</q-item-label>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CompanyCard',
+};
+</script>
+
+<style lang="scss">
+.container {
+  border: 2px solid;
+  border-radius: 10px;
+  padding: 0.62rem 1rem;
+  height: 4rem;
+  width: 17rem;
+}
+</style>
