47d165b49a4b7b9e9bec5b7db6d6c197e50e811e
luisescx <luis.esculapio@live.com>
2022-05-20 16:12:20 -0300

    feat: added header component

---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-header class="bg-accent">
+      <q-toolbar class="toolbar-container">
+        <q-icon
+          name="img:icons/logo.svg"
+          size="50px"
+          class="q-pl-md"
+        />
+
+        <div class="row q-px-lg toolbar-card-container">
+          <CompanyCard />
+
+          <q-icon
+            name="img:icons/bell.svg"
+            size="32px"
+          />
+        </div>
+
+        <q-separator vertical dark/>
+
+        <div class="q-px-lg">Olá, Joaquim</div>
+
+        <q-icon
+          name="img:icons/user.svg"
+          size="54px"
+        />
+      </q-toolbar>
+    </q-header>
+</template>
+
+<script>
+import CompanyCard from 'src/components/CompanyCard.vue';
+
+export default {
+  name: 'Header',
+  components: {
+    CompanyCard,
+  },
+};
+</script>
+
+<style lang="scss">
+.toolbar-container {
+  min-height: 80px;
+  max-height: 80px;
+}
+
+.toolbar-card-container {
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-top: 0.68rem;
+  margin-bottom: 0.68rem
+}
+</style>
