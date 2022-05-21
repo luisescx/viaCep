b6d283b028fa427a2ed966ab1ef44b89dd1b5f59
luisescx <luis.esculapio@live.com>
2022-05-20 22:39:45 -0300

    feat: added ZipCodeCard component

---
+++ src/components/ZipCodeCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="row address">
+    <q-icon
+      name="img:icons/place.svg"
+      size="md"
+      class="q-mr-lg"
+    />
+
+    <div>
+      <p class="text text-weight-bold">
+        <span class="text-dark">CEP </span>{{ zipCodeFormatted }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { formatZipCode } from 'src/utils';
+
+export default {
+  name: 'ZipCodeCard',
+  props: {
+    zipCode: String,
+  },
+  data() {
+    return {
+      zipCodeFormatted: '',
+    };
+  },
+  mounted() {
+    this.zipCodeFormatted = formatZipCode(this.zipCode);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .text {
+    font-size: 1.25rem;
+    color: #B7B7B7;
+  }
+</style>
